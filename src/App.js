8828db685a0ebb168b28b859b8dd8bcb4e48da79
Oleksii Halenko <axe.halenko@gmail.com>
2021-09-07 23:39:35 +0300

    add postlist and post items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React, { useState } from 'react';
+import PostList from './components/PostList';
+import './styles/App.css';
 
 function App() {
 
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript 1', body: 'Javascript - programming language' },
+    { id: 2, title: 'Javascript 2', body: 'Javascript - programming language' },
+    { id: 3, title: 'Javascript 3', body: 'Javascript - programming language' },
+  ])
+
   return (
     <div className="App">
-      <Counter />
-      <Counter />
-      <Counter />
+      <PostList posts={posts} title='Posts for JS' />
     </div>
   );
 }
